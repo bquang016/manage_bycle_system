1b0416290e7170c0461903cb9d222ef1ad8e5e6a
bquang016 <buidangquangmtp@gmail.com>
2025-10-14 22:33:35 +0700

    first commit

---
+++ src/main/java/com/example/managebyclesystem/ManageBycleSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.managebyclesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageBycleSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManageBycleSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/managebyclesystem/ManageBycleSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.managebyclesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageBycleSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
