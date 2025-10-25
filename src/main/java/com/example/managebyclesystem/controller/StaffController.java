4507029eddaa38882ea9d685280deddf4db88f67
Hoang de <apksuyuki1@gmail.com>
2025-10-25 09:14:31 +0700

    sua ten phuong thuc edit

--- src/main/java/com/example/managebyclesystem/controller/StaffController.java
+++ src/main/java/com/example/managebyclesystem/controller/StaffController.java
@@ -69,7 +69,7 @@ public class StaffController {
     }
 
 
-    @PostMapping("/update")
+    @PostMapping("/edit/{id}")
     public String updateStaff(@ModelAttribute Staff staff, Model model) {
         try {
             staffService.updateStaff(staff);
